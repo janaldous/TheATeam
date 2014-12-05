29fea298f307c4a975f528aeefe495eafcde312a
Torasian <williamhawken@Williams-MacBook-Pro.local>
2014-12-05 05:58:36 +0000

    f
    f

--- Unicef/table.php
+++ Unicef/table.php
@@ -11,10 +11,10 @@ $result = mysql_query($query);
 echo "<table border = 1>"; // start a table tag in the HTML
 
 while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
-echo "<tr><td>" . $row['firstname'] . "</td><td>" . $row['lastname'] . "</td></tr>" . "<tr><td>" . $row['age'] . "<tr><td>" . $row['lastseenplace'] . "</td><td>" . "<tr><td>" . $row['lastseentime'] . "</td><td>" //$row['index'] the index here is a field name
+echo "<tr><td>" . $row['firstname'] . "</td><td>" . $row['lastname'] . "</td></tr>" 
 }
 
-echo "</table>"; //Close the table in HTML
+echo "</tablfe>"; //Close the table in HTML
 
 mysql_close(); //Make sure to close out the database connection
 
