850a59df8e644d5b5050b30a237b38c661f07284
Torasian <williamhawken@Williams-MacBook-Pro.local>
2014-12-05 05:41:41 +0000

    ca
    ca

--- Unicef/table.php
+++ Unicef/table.php
@@ -8,7 +8,7 @@ mysql_select_db('unification');
 $query = "SELECT * FROM missing"; //You don't need a ; like you do in SQL
 $result = mysql_query($query);
 
-echo "<table>"; // start a table tag in the HTML
+echo "<table border = 1>"; // start a table tag in the HTML
 
 while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
 echo "<tr><td>" . $row['firstname'] . "</td><td>" . $row['lastname'] . "</td></tr>" . "<tr><td>" . $row['age'] . "<tr><td>" . $row['lastseenplace'] . "</td><td>" . "<tr><td>" . $row['lastseentime'] . "</td><td>" //$row['index'] the index here is a field name
