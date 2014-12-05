cab13d5db93dc1e8365c203f53d5ef8c6a0259f5
shubham145 <shubham1445>
2014-12-05 00:04:30 +0000

    login
    login

---
+++ Unicef/login.php
@@ -0,0 +1,40 @@
+
+<?php
+
+$host="db4free.net"; // Host name 
+$username="ateam"; // Mysql username 
+$password="shubham"; // Mysql password 
+$db_name="unification"; // Database name 
+$tbl_name="login"; // Table name 
+
+// Connect to server and select databse.
+mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysql_select_db("$db_name")or die("cannot select DB");
+
+// username and password sent from form 
+$username=$_POST['username']; 
+$password=$_POST['password']; 
+
+// To protect MySQL injection (more detail about MySQL injection)
+$username = stripslashes($username);
+$password = stripslashes($password);
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+$sql="SELECT * FROM $tbl_name WHERE username='$username' and password='$password'";
+$result=mysql_query($sql);
+
+// Mysql_num_row is counting table row
+$count=mysql_num_rows($result);
+
+// If result matched $myusername and $mypassword, table row must be 1 row
+if($count==1){
+
+// Register $myusername, $mypassword and redirect to file "login_success.php"
+session_register("username");
+session_register("password"); 
+header("location:login_success.html");
+}
+else {
+echo "Wrong Username or Password";
+}
+?>
